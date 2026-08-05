c40a5914262cc0473ea91c063062ce810382b23f
Lukasz <lukasz@colorsnake.local>
2026-08-05 22:33:48 +0200

    feat: Additional physics types and framework config

---
+++ rocket_sil_framework/include/physics/physics_types.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <eigen3/Eigen/Dense>
+
+struct EngineOutput {
+    Eigen::Vector3d thrust_body; // N
+    double mass_flow_rate;       // kg/s (positive value meaning mass leaving the vehicle)
+};
+
+struct MassProperties {
+    double total_mass;           // kg
+    Eigen::Vector3d center_of_gravity; // m (relative to vehicle datum)
+    Eigen::Matrix3d inertia_tensor;    // kg*m^2
+};
+
+struct AeroForces {
+    Eigen::Vector3d aerodynamic_force_body;  // N
+    Eigen::Vector3d aerodynamic_moment_body; // Nm
+};
